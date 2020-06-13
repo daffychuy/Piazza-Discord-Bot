19be9dec4eaf8d2ec7c5f309d19c9b8a7cd6b6dd
Zachary Zulanas <zzulanas@gmail.com>
2020-06-13 15:17:52 -0700

    basic discord connectivity

---
+++ connect.py
@@ -0,0 +1,41 @@
+import os
+
+import discord
+***REMOVED***
+import random
+
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
